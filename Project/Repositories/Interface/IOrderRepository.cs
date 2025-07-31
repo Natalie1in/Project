803b4a7e96f48a4b2c107fd91f11e9b909ff6226
Natalie1in <natalielin001@gmail.com>
2025-07-31 23:43:05 +0800

    feat: 新增 Order 的 Repository

---
+++ Project/Repositories/Interface/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Project.Models;
+
+namespace Project.Repositories.@interface
+{
+    public interface IOrderRepository
+    {
+        Task<List<Order>> GetAllAsync();
+        Task<Order?> GetByIdAsync(int id);
+        Task AddAsync(Order order);
+        Task UpdateAsync(Order order);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Project/Repositories/OrderRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Models;
+using Project.Repositories.@interface;
+
+namespace Project.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly NorthwindContext _context;
+        public OrderRepository(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Order>> GetAllAsync()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        public async Task<Order?> GetByIdAsync(int id)
+        {
+            return await _context.Orders.FindAsync(id);
+        }
+
+        public async Task AddAsync(Order order)
+        {
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Order order)
+        {
+            _context.Orders.Update(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order != null)
+            {
+                _context.Orders.Remove(order);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
