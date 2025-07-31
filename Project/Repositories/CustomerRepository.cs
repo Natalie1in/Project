ed7aaed91eef58dfc00caedc96b47b6d07ca5c57
Natalie1in <natalielin001@gmail.com>
2025-08-01 00:14:51 +0800

    feat: 新增 Customer 的 Repository

---
+++ Project/Repositories/CustomerRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Models;
+using Project.Repositories.Interface;
+
+namespace Project.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly NorthwindContext _context;
+        public CustomerRepository(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Customer>> GetAllAsync()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        public async Task<Customer?> GetByIdAsync(string customerId)
+        {
+            return await _context.Customers.FindAsync(customerId);
+        }
+
+        public async Task AddAsync(Customer customer)
+        {
+            await _context.Customers.AddAsync(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Customer customer)
+        {
+            _context.Customers.Update(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(string customerId)
+        {
+            var customer = await _context.Customers.FindAsync(customerId);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Project/Repositories/Interface/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Project.Models;
+
+namespace Project.Repositories.Interface
+{
+    public interface ICustomerRepository
+    {
+        Task<List<Customer>> GetAllAsync();
+        Task<Customer?> GetByIdAsync(string customerId);
+        Task AddAsync(Customer customer);
+        Task UpdateAsync(Customer customer);
+        Task DeleteAsync(string customerId);
+    }
+}
