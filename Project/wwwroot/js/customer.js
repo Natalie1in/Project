d328faad30e3db602252f23346cbcc57134eb503
Natalie1in <natalielin001@gmail.com>
2025-08-01 01:24:06 +0800

    chore: 新增 JS 檔並於 Layout 引入 Axios

---
+++ Project/wwwroot/js/customer.js
@@ -0,0 +1,14 @@
+﻿function updateQty(orderId, productId) {
+    const inputId = `qty-${orderId}-${productId}`;
+    const quantity = document.getElementById(inputId).value;
+
+    axios.post('/Customer/UpdateProductQuantity', {
+        orderId: orderId,
+        productId: productId,
+        quantity: parseInt(quantity)
+    }).then(res => {
+        alert(res.data.message);
+    }).catch(err => {
+        alert("更新失敗：" + err.response?.data || err.message);
+    });
+}
